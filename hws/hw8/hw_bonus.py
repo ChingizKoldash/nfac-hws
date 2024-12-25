eff2c8f8855be3a8f9336b7ed6ca30b01f42726f
Shyngys Koldash <ytleugazy@dar.kz>
2024-12-25 22:50:05 +0500

    feat : homework 8

---
+++ hws/hw8/hw_bonus.py
@@ -0,0 +1,78 @@
+"""
+💎 Exercise-1 (Longest Consecutive Sequence):
+Write a function "longest_consecutive(my_list: list[int]) -> int" that takes a 
+list of integers and returns the length of the longest consecutive elements 
+sequence in the list. The list might be unsorted.
+
+Example:
+
+longest_consecutive([100, 4, 200, 1, 3, 2]) -> 4
+"""
+
+def longest_consecutive(my_list: list[int]) -> int:
+    if not my_list:
+        return 0
+    
+    my_list.sort()
+    
+    longest = 1
+    current_streak = 1
+    
+    for i in range(1, len(my_list)):
+        if my_list[i] == my_list[i - 1] + 1:  
+            current_streak += 1
+        elif my_list[i] != my_list[i - 1]:  
+            longest = max(longest, current_streak)
+            current_streak = 1
+    
+    return max(longest, current_streak)
+    pass
+
+"""
+💎 Exercise-2 (Find missing number):
+Write a function "find_missing(my_list: list[int]) -> int" that takes a 
+list of integers from 1 to n. The list can be unsorted and have one 
+number missing. The function should return the missing number.
+
+Example:
+
+find_missing([1, 2, 4]) -> 3
+"""
+
+def find_missing(my_list: list[int]) -> int:
+    # write your code here
+    pass
+
+
+"""
+💎 Exercise-3 (Find duplicate number):
+Write a function "find_duplicate(my_list: list[int]) -> int" that takes a list 
+of integers where each integer is in the range of 1 to n (n is the size of the list). 
+The list can have one number appearing twice and the function should return this number.
+
+Example:
+
+find_duplicate([1, 3, 4, 2, 2]) -> 2
+"""
+
+def find_duplicate(my_list: list[int]) -> int:
+    # write your code here
+    pass
+
+
+"""
+💎 Exercise-4 (Group Anagrams):
+Write a function "group_anagrams(words: list[str]) -> list[list[str]]" that 
+takes a list of strings (all lowercase letters), groups the anagrams together, 
+and returns a list of lists of grouped anagrams.
+
+An Anagram is a word or phrase formed by rearranging the letters of 
+a different word or phrase, typically using all the original letters exactly once.
+
+group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) 
+-> [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
+"""
+
+def group_anagrams(words: list[str]) -> list[list[str]]:
+    # write your code here
+    pass
